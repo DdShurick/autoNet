561a31771582a0b82d4cc3268e0c3087478024ff
DdShurick <DdShurick@mail.ru>
2019-06-20 21:50:28 +0000

    simplified check3Gtty

--- src/check3Gtty.c
+++ src/check3Gtty.c
@@ -1,34 +1,20 @@
-/*check3Gtty GPL v2, DdShurick 06.12.2017*/
 #include <stdio.h>
 #include <string.h>
-#include <dirent.h>
 #include <stdlib.h>
-#include <unistd.h> 
-#include <sys/types.h>
+#include <unistd.h>
 #include <signal.h>
 
-char 	str[32]="", dev[16]="/dev/", mdir[42]="/sys/bus/usb-serial/devices/";
-FILE	*fd;
-DIR		*md;
-struct	dirent *entry;
-
 int main(int argc, char **argv) {
-	
-	int n=0;
+	char 	str[9]="", dev[16]="/dev/";
+	FILE	*fd;
 	pid_t pid;
-	char buf[14]="";
 	
 	if (!argv[1]) exit(1);
 	
 	strcat(dev,argv[1]);
-	strcat(mdir,argv[1]);
-	strcat(mdir,"/../");
 	
-	if ((fd = fopen(dev,"r+"))==NULL) { 
-		exit(1); 
-	}
+	if ((fd = fopen(dev,"r+"))==NULL) exit(1); 
 	
-	sleep(1);
 	fputs("AT\n",fd);
 	
 	switch(pid=fork()) {
@@ -41,30 +27,22 @@ int main(int argc, char **argv) {
 			raise(15);
 		default:
 			if (fgetc(fd)) {
+				while (1) {
+					fgets(str,9,fd);
+					if (str[0] == '^') {
+						unlink("/dev/modem_cli");
+						symlink(dev,"/dev/modem_cli");
+						break;
+					}
+					if ((strstr(str,"OK")) != NULL) {
+						unlink("/dev/modem");
+						symlink(dev,"/dev/modem");
+						break;
+					}
+				}
 				fclose(fd);
 				kill(pid,15);
-			}	
+			}
 	}
-	
-	md=opendir(mdir);
-	while ((entry = readdir(md))!=0) {
-		if (strstr(entry->d_name,"ep_")!=0 ) {
-			n = n + 1;
-		}
-	}
-	if (n==2) {
-		closedir(md);
-		printf("modem_cli\n");
-		unlink("/dev/modem_cli");
-		symlink(dev,"/dev/modem_cli");
-		exit(0);
-	} 
-	if (n==3) {
-		closedir(md);
-		printf("modem\n");
-		unlink("/dev/modem");
-		symlink(dev,"/dev/modem");
-		exit(0);
-	}
+	exit (0);
 }
-
