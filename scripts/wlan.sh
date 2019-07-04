7739c63537e7d49f7f07be81c5c7466d2679eb2a
DdShurick <DdShurick@mail.ru>
2019-07-04 12:00:46 +0000

    rewrited wlan.sh

--- scripts/wlan.sh
+++ scripts/wlan.sh
@@ -9,11 +9,6 @@ HWADDR=$(cat /sys/class/net/$IFACE/address)
 
 [ -f /tmp/iwopen ] && rm /tmp/iwopen
 
-ifdown () {
-	kill $(/bin/pidof wpa_supplicant)
-	ifconfig $IFACE down
-}
-
 #Получаем список точек доступа и ищем файл конфигурации
 if ifup; then
 	echo "$IFACE up" | tee /var/log/$IFACE.log
@@ -23,34 +18,42 @@ if [ ! -s /tmp/iwlist ]; then
 	IMG=wireless_err
 	msg_err $IFACE "Сети wifi не найдены"
 	ifdown
+	exit 0
 fi
-for WLNADDR in $(awk '/Address:/ {print $5}' /tmp/iwlist)
-do
+[ $(awk '/Address:/ {print $5}' /tmp/iwlist | wc -w) -gt 1 ] && exec wifi noscan
+#for WLNADDR in $(awk '/Address:/ {print $5}' /tmp/iwlist)
+#do
+WLNADDR=$(awk '/Address:/ {print $5}' /tmp/iwlist)
 	if [ "$(grep -A 3 $WLNADDR /tmp/iwlist | grep 'Encryption key:off')" ];then
-	 grep -A 4 $WLNADDR /tmp/iwlist | tee -a /tmp/iwopen #Запомним открытые
-	else 
-		if [ -f ${WPADIR}${WLNADDR}.wpa.conf ];then
-			WPA_CONF="$(ls ${WPADIR}${WLNADDR}.wpa.conf)" #???
-			ESSID=$(awk -F \" '/ESSID/ {print $2}' $WPA_CONF)
-			echo $WPA_CONF | tee -a /var/log/${IFACE}.log #контроль
-			if wpa_supplicant -B -D nl80211 -i "$IFACE" -c "${WPADIR}${WLNADDR}.wpa.conf"; then
-				echo "wpa_supplicant ok" | tee -a /var/log/$IFACE.log 
-				ST=ok
-				break
-			fi
-		else
-			continue
-		fi
-	fi 
-	if [ -s /tmp/iwopen ]; then
+#	 grep -A 4 $WLNADDR /tmp/iwlist | tee -a /tmp/iwopen #Запомним открытые
 	 MAXLEVEL=$(grep -A 2 $WLNADDR /tmp/iwopen | grep 'Quality' | cut -f2 -d '-' | sort | head -n 1)
 	 ESSID=$(grep -A 2 "$MAXLEVEL" /tmp/iwopen | grep ESSID | cut -f2 -d ':' | tr -d '"')
 	 CHANNEL=$(grep -B 1 "$MAXLEVEL" /tmp/iwopen | grep Channel | cut -f2 -d ':')
 	 iwconfig $IFACE essid $ESSID key off channel $CHANNEL && ST=ok
-	 echo "$IFACE essid $ESSID key off channel $CHANNEL" | tee -a /var/log/$IFACE.log #контроль
-	fi
-done
-if [ "$ST" = "" ]; then
+	 echo "$IFACE essid $ESSID key off channel $CHANNEL" | tee -a /var/log/$IFACE.log
+	else 
+		if [ -f ${WPADIR}${WLNADDR}.wpa.conf ];then
+			WPA_CONF="${WPADIR}${WLNADDR}.wpa.conf"
+			ESSID=$(awk -F \" '/ESSID/ {print $2}' $WPA_CONF)
+			echo $WPA_CONF | tee -a /var/log/${IFACE}.log #контроль
+			if wpa_supplicant -B -D nl80211 -i "$IFACE" -c "${WPADIR}${WLNADDR}.wpa.conf"; then
+				echo "wpa_supplicant ok" | tee -a /var/log/$IFACE.log 
+				WS=ok
+#				break
+			fi
+#		else
+#			continue
+		fi
+	fi 
+#	if [ -s /tmp/iwopen ]; then
+#	 MAXLEVEL=$(grep -A 2 $WLNADDR /tmp/iwopen | grep 'Quality' | cut -f2 -d '-' | sort | head -n 1)
+#	 ESSID=$(grep -A 2 "$MAXLEVEL" /tmp/iwopen | grep ESSID | cut -f2 -d ':' | tr -d '"')
+#	 CHANNEL=$(grep -B 1 "$MAXLEVEL" /tmp/iwopen | grep Channel | cut -f2 -d ':')
+#	 iwconfig $IFACE essid $ESSID key off channel $CHANNEL && ST=ok
+#	 echo "$IFACE essid $ESSID key off channel $CHANNEL" | tee -a /var/log/$IFACE.log #контроль
+#	fi
+#done
+if [ "$WS" = "" ]; then
 	exec wifi
 else
  T=0
