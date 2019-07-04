c2473d8fe80998d02d56dbe35efd149fd36ee1e6
DdShurick <DdShurick@mail.ru>
2019-07-04 05:57:05 +0000

    del full path, add PATH

--- scripts/lan.sh
+++ scripts/lan.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#DdShurick 25.08.18
+#DdShurick GPL v2
 [ $1 ] && IFACE=$1 || exit 1
 
 case $IFACE in
@@ -14,10 +14,10 @@ HWADDR=$(/bin/cat /sys/class/net/$IFACE/address)
 pppoeup () {
 if [ -f ${CONFDIR}${HWADDR}.pppoe.conf ]; then
 	. ${CONFDIR}${HWADDR}.pppoe.conf
-	/sbin/modprobe pppoe
-	[ /bin/grep "$LOGIN" /etc/ppp/chap-secrets ] || /bin/echo "$LOGIN	*	$PASSWD	$IP" >> /etc/ppp/chap-secrets
-	[ /bin/grep "$LOGIN" /etc/ppp/pap-secrets ] || /bin/echo "$LOGIN	*	$PASSWD	$IP" >> /etc/ppp/pap-secrets
-	/bin/echo "plugin rp-pppoe.so
+	modprobe pppoe
+	[ grep "$LOGIN" /etc/ppp/chap-secrets ] || echo "$LOGIN	*	$PASSWD	$IP" >> /etc/ppp/chap-secrets
+	[ grep "$LOGIN" /etc/ppp/pap-secrets ] || /bin/echo "$LOGIN	*	$PASSWD	$IP" >> /etc/ppp/pap-secrets
+	echo "plugin rp-pppoe.so
 $AC
 $SN
 $IFACE
@@ -27,21 +27,21 @@ persist
 defaultroute
 hide-password
 noauth
-" | /usr/bin/tee /etc/ppp/peers/$NAME
-	[ "$DNS1" != "" -o "$DNS1" != "0.0.0.0" ] && /bin/echo "nameserver $DNS1" | tee /etc/resolv.conf
-	[ "$DNS2" != "" -o "$DNS2" != "0.0.0.0" ] && /bin/echo "nameserver $DNS2" | tee -a /etc/resolv.conf
-	/usr/sbin/pppd call $NAME
+" | tee /etc/ppp/peers/$NAME
+	[ "$DNS1" != "" -o "$DNS1" != "0.0.0.0" ] && echo "nameserver $DNS1" | tee /etc/resolv.conf
+	[ "$DNS2" != "" -o "$DNS2" != "0.0.0.0" ] && echo "nameserver $DNS2" | tee -a /etc/resolv.conf
+	pppd call $NAME
 	PID=$!
-	/bin/sleep 3
+	sleep 3
 	if [ -h /sys/class/net/ppp0 ]; then
-		/bin/echo "pppoe connect" | /usr/bin/tee -a /var/log/$IFACE.log
-		/bin/cp /etc/ppp/resolv.conf /etc/resolv.conf
+		echo "pppoe connect" | tee -a /var/log/$IFACE.log
+		cp /etc/ppp/resolv.conf /etc/resolv.conf
 		IMG=connect
 		msg "$IFACE PPPoE up"
 		return 0
 	else
-		/bin/kill $PID
-		echo "No PPPoE connect" | /usr/bin/tee -a /var/log/${1}.log
+		kill $PID
+		echo "No PPPoE connect" | tee -a /var/log/${1}.log
 		return 1
 	fi
 else
@@ -49,28 +49,27 @@ else
 fi
 }
 
-ifup && /bin/echo "$0: $IFACE up" | /usr/bin/tee /var/log/${IFACE}.log
+ifup && echo "$0: $IFACE up" | tee /var/log/${IFACE}.log
 
-if [ "$(/usr/bin/cat /sys/class/net/$IFACE/carrier)" = 1 ]; then
-	/bin/echo "$0: carrier yes" | /usr/bin/tee -a /var/log/${IFACE}.log
+if [ "$(cat /sys/class/net/$IFACE/carrier)" = 1 ]; then
+	echo "$0: carrier yes" | tee -a /var/log/${IFACE}.log
 	if [ -s "${CONFDIR}${HWADDR}.pppoe.conf" -o "$2" = "pppoeup" ]; then
 		pppoeup $IFACE
 		ST=$?
-		[ "$ST" = 1 ] && /bin/kill $(/bin/pidof pppd)
+		[ "$ST" = 1 ] && kill $(pidof pppd)
 	fi
 	if [ -s "${CONFDIR}${HWADDR}.conf" ]; then
 		static
 	else
-		dhcpc #$(/sbin/udhcpc -i $IFACE -n 2>/dev/null)
-#		/sbin/udhcpc -n -i $IFACE | /usr/bin/tee -a /var/log/$IFACE.log
+		dhcpc
 		if [ $? = 1 -a $ST = 1 ]; then
-			/sbin/ifconfig $IFACE down
-			/bin/echo "$0: $IFACE down" | /usr/bin/tee -a /var/log/$IFACE.log
+			ifconfig $IFACE down
+			echo "$0: $IFACE down" | tee -a /var/log/$IFACE.log
 			msg_err $IFACE "$IFACE down"
 		fi
 	fi
 else
-	"$0: $IFACE: No carrier" | echo tee -a /var/log/${1}.log
+	echo "$0: $IFACE: No carrier" | echo tee -a /var/log/${1}.log
 	msg_err $IFACE "No carrier"
 	exit 1
 fi
--- scripts/wlan.sh
+++ scripts/wlan.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
-#DdShurick 12.08.18
+#DdShurick GPL v2
 [ $1 ] && IFACE=$1 || exit 1
 IMG="wireless_"
 . /usr/lib64/upNet/libupNet
 WPADIR="/etc/net/wpa_profiles/"
 CONFDIR="/etc/net/interfaces/"
-HWADDR=$(/bin/cat /sys/class/net/$IFACE/address)
+HWADDR=$(cat /sys/class/net/$IFACE/address)
 
-[ -f /tmp/iwopen ] && /bin/rm /tmp/iwopen
+[ -f /tmp/iwopen ] && rm /tmp/iwopen
 
 ifdown () {
-	/bin/kill $(/bin/pidof wpa_supplicant)
-	/sbin/ifconfig $IFACE down
+	kill $(/bin/pidof wpa_supplicant)
+	ifconfig $IFACE down
 }
 
 #Получаем список точек доступа и ищем файл конфигурации
 if ifup; then
-	/bin/echo "$IFACE up" | tee /var/log/$IFACE.log
-	/usr/bin/iwlist $IFACE scan | /bin/egrep 'Address:|Channel:|Quality|Encryption key:|ESSID:' | /usr/bin/tee /tmp/iwlist #контроль
+	echo "$IFACE up" | tee /var/log/$IFACE.log
+	iwlist $IFACE scan | egrep 'Address:|Channel:|Quality|Encryption key:|ESSID:' | tee /tmp/iwlist #контроль
 fi
 if [ ! -s /tmp/iwlist ]; then 
 	IMG=wireless_err
 	msg_err $IFACE "Сети wifi не найдены"
 	ifdown
 fi
-for WLNADDR in $(/usr/bin/awk '/Address:/ {print $5}' /tmp/iwlist)
+for WLNADDR in $(awk '/Address:/ {print $5}' /tmp/iwlist)
 do
-	if [ "$(/bin/grep -A 3 $WLNADDR /tmp/iwlist | /bin/grep 'Encryption key:off')" ];then
-	 /bin/grep -A 4 $WLNADDR /tmp/iwlist | tee -a /tmp/iwopen #Запомним открытые
+	if [ "$(grep -A 3 $WLNADDR /tmp/iwlist | grep 'Encryption key:off')" ];then
+	 grep -A 4 $WLNADDR /tmp/iwlist | tee -a /tmp/iwopen #Запомним открытые
 	else 
 		if [ -f ${WPADIR}${WLNADDR}.wpa.conf ];then
-			WPA_CONF="$(/bin/ls ${WPADIR}${WLNADDR}.wpa.conf)" #???
-			ESSID=$(/usr/bin/awk -F \" '/ESSID/ {print $2}' $WPA_CONF)
-			/bin/echo $WPA_CONF | tee -a /var/log/${IFACE}.log #контроль
-			if /usr/sbin/wpa_supplicant -B -D nl80211 -i "$IFACE" -c "${WPADIR}${WLNADDR}.wpa.conf"; then
-				echo "wpa_supplicant ok" | /usr/bin/tee -a /var/log/$IFACE.log 
+			WPA_CONF="$(ls ${WPADIR}${WLNADDR}.wpa.conf)" #???
+			ESSID=$(awk -F \" '/ESSID/ {print $2}' $WPA_CONF)
+			echo $WPA_CONF | tee -a /var/log/${IFACE}.log #контроль
+			if wpa_supplicant -B -D nl80211 -i "$IFACE" -c "${WPADIR}${WLNADDR}.wpa.conf"; then
+				echo "wpa_supplicant ok" | tee -a /var/log/$IFACE.log 
 				ST=ok
 				break
 			fi
@@ -43,24 +43,24 @@ do
 		fi
 	fi 
 	if [ -s /tmp/iwopen ]; then
-	 MAXLEVEL=$(/bin/grep -A 2 $WLNADDR /tmp/iwopen | /bin/grep 'Quality' | cut -f2 -d '-' | sort | head -n 1)
-	 ESSID=$(/bin/grep -A 2 "$MAXLEVEL" /tmp/iwopen | /bin/grep ESSID | cut -f2 -d ':' | tr -d '"')
-	 CHANNEL=$(/bin/grep -B 1 "$MAXLEVEL" /tmp/iwopen | /bin/grep Channel | cut -f2 -d ':')
-	 /usr/bin/iwconfig $IFACE essid $ESSID key off channel $CHANNEL && ST=ok
-	 /bin/echo "$IFACE essid $ESSID key off channel $CHANNEL" | tee -a /var/log/$IFACE.log #контроль
+	 MAXLEVEL=$(grep -A 2 $WLNADDR /tmp/iwopen | grep 'Quality' | cut -f2 -d '-' | sort | head -n 1)
+	 ESSID=$(grep -A 2 "$MAXLEVEL" /tmp/iwopen | grep ESSID | cut -f2 -d ':' | tr -d '"')
+	 CHANNEL=$(grep -B 1 "$MAXLEVEL" /tmp/iwopen | grep Channel | cut -f2 -d ':')
+	 iwconfig $IFACE essid $ESSID key off channel $CHANNEL && ST=ok
+	 echo "$IFACE essid $ESSID key off channel $CHANNEL" | tee -a /var/log/$IFACE.log #контроль
 	fi
 done
 if [ "$ST" = "" ]; then
-	exec /usr/bin/wifi
+	exec wifi
 else
  T=0
-	until [ "$(/bin/cat /sys/class/net/$IFACE/carrier)" = 1 ] #???
+	until [ "$(cat /sys/class/net/$IFACE/carrier)" = 1 ] #???
 	do
-		/bin/sleep 1
-		T="$(/usr/bin/expr $T + 1)"
-	 	/bin/echo -n "$T " | tee -a /var/log/$IFACE.log #контроль
+		sleep 1
+		T="$(expr $T + 1)"
+	 	echo -n "$T " | tee -a /var/log/$IFACE.log #контроль
 		if [ $T = 10 ]; then
-			/bin/echo "$0: Timeout, interface $IFACE down" | tee -a /var/log/$IFACE.log контроль
+			echo "$0: Timeout, interface $IFACE down" | tee -a /var/log/$IFACE.log контроль
 			IMG=wireless_err
 			msg_err "Timeout, interface $IFACE down"
 		fi 
@@ -69,11 +69,10 @@ else
 	if [ -s ${CONFDIR}${HWADDR}.conf ]; then
 		static
 	else
-		dhcpc #$(/sbin/udhcpc -i $IFACE -n 2>/dev/null)
-#		/sbin/udhcpc -n -i $IFACE | /usr/bin/tee -a /var/log/$IFACE.log
+		dhcpc
 		if [ $? = 1 ]; then
 			ifdown
-			/bin/echo "$0: $IFACE down" | tee -a /var/log/$IFACE.log
+			echo "$0: $IFACE down" | tee -a /var/log/$IFACE.log
 			IMG=wireless_err
 			msg_err $IFACE "$IFACE down"
 		fi
